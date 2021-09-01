acf684ba4cc5f5bd2b1a55c6cba6bea410548699
sh3ngsh3ng <yshenglim13@gmail.com>
2021-09-01 17:52:41 +0000

    v3

--- hands-on/q9-book-search/script.js
+++ hands-on/q9-book-search/script.js
@@ -3,7 +3,16 @@
 
 let BASE_URL = "http://openlibrary.org/search.json?"
 
-
+document.querySelector('#btn').addEventListener('click', async function() {
+    let userInput = document.querySelector('#user-input').value
+    console.log(userInput)
+    
+    // let response = await axios.get(BASE_URL, {
+    //     params: {
+    //         title: //variable with ++++ in title
+    //     }
+    // })
+})
 
 
 
