a95bc835211482c5ebf8edb979a191bc9ea21d9b
sh3ngsh3ng <yshenglim13@gmail.com>
2021-09-01 18:28:50 +0000

    v1

--- hands-on/q9-book-search/script.js
+++ hands-on/q9-book-search/script.js
@@ -1,17 +1,25 @@
 
 
 
-let BASE_URL = "http://openlibrary.org/search.json?"
+let BASE_URL = "http://openlibrary.org/search.json?title="
 
 document.querySelector('#btn').addEventListener('click', async function() {
     let userInput = document.querySelector('#user-input').value
-    console.log(userInput)
     
-    // let response = await axios.get(BASE_URL, {
-    //     params: {
-    //         title: //variable with ++++ in title
-    //     }
-    // })
+    let x = userInput.replace(/ /g, "+")
+    let endpoint = BASE_URL + x
+
+    let response = await axios.get(endpoint)
+    let mainObj = response.data
+    let docObj = mainObj.docs
+    let bookList = docObj.docs // an array
+
+    for (let i = 0; i < bookList.length; i++) {
+        let eachBook = bookList[i]
+        let title = eachBook.title
+        let author = eachBook.author_name
+        
+    }
 })
 
 
